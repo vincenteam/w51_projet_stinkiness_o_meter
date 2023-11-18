ef52ee49c87a92ed7bb4681faaec828f1f08d6af
Vincent <bardou.vincent@free.fr>
2023-11-18 17:35:31 +0100

    add 'pagination' & timeout when searching animes

--- stink/src/routes/search.jsx
+++ stink/src/routes/search.jsx
@@ -1,8 +1,10 @@
 import { Form, Outlet, useLoaderData, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { parseString } from "xml2js";
 import { Anime } from "./anime.jsx";
 import { LinkWithQuery } from "../linkWithQuery";
+
+import "./search.css";
 
 export function searchAnimesLoader({ request }) {
   let url = new URL(request.url);
@@ -20,6 +22,7 @@ export function searchAnimesLoader({ request }) {
       return response.json();
     })
     .then((animeIds) => {
+      return { ids: animeIds.ids, search: searchTerm };
       // Fetch-ception to get the other infos on the animes that match the searchTerm
       if (animeIds.length === 0) {
         // If the list of ids is empty, return an empty list
@@ -30,17 +33,18 @@ export function searchAnimesLoader({ request }) {
         let promiseList = [];
         for (let id of animeIds.ids) {
           promiseList.push(
-            fetch("http://localhost:4200/animeInfo?id=" + id).then((data) => {
-              return data.json();
-            }).catch(function(err)
-            {
-               return null
-            })
+            fetch("http://localhost:4200/animeInfo?id=" + id)
+              .then((data) => {
+                return data.json();
+              })
+              .catch(function (err) {
+                return null;
+              })
           );
         }
         return Promise.all(promiseList).then((aniData) => {
           for (let anime of aniData) {
-            if (anime){
+            if (anime) {
               animeList.push(anime);
             }
           }
@@ -50,13 +54,84 @@ export function searchAnimesLoader({ request }) {
     });
 }
 
-export function Animes({addAnime}) {
+export function Animes({ addAnime }) {
   let loaded = useLoaderData();
-  const navigate = useNavigate();
+  const [idsToLoad, setIdsToLoad] = useState([]);
+  const [animes, setAnimes] = useState([]);
+  const [canRequest, setCanRequest] = useState(true);
 
-  function onSelectAnime(){
+  let requestTimeOut = 10; // timeout of requests to backend
+  const loadGroupSize = 10;
 
+  function checkScroll(event) {
+    const elem = event.target;
+    if (elem.scrollTop + elem.clientHeight >= elem.scrollHeight) {
+      setIdsToLoad(idsToLoad.slice(loadGroupSize));
+      loadAnimeInfo(animes, idsToLoad.slice(0, loadGroupSize));
+    }
   }
+
+  function loadAnimeInfo(baseAnime, idsList) {
+    // create promises to get info on the animes based on ids on the state
+    console.log("loaded", animes.length)
+    if (!canRequest) {
+      console.log("in timeout");
+      return null;
+    }
+    setCanRequest(false);
+    const promiseList = [];
+    for (let id of idsList) {
+      promiseList.push(
+        fetch("http://localhost:4200/animeInfo?id=" + id)
+          .then((data) => {
+            return data.json();
+          })
+          .catch(function (err) {
+            return null;
+          })
+      );
+    }
+
+    console.log("promises", promiseList);
+
+    if (promiseList.length !== 0) {
+      Promise.all(promiseList).then((aniData) => {
+        // get all the results, filter nulls
+        const animeList = [];
+        for (let anime of aniData) {
+          if (anime) {
+            animeList.push(anime);
+          }
+        }
+        // add new results to state, avoid duplicates
+        setAnimes((_) => {
+          const ids = baseAnime.map((a) => a.id);
+
+          const tmp = [...baseAnime];
+          for (const a of animeList) {
+            if (!ids.includes(a.id)) {
+              tmp.push(a);
+            }
+          }
+          console.log("set with", tmp);
+          return tmp;
+        });
+      });
+    }
+  }
+  console.log("ids", loaded.ids);
+  useEffect(() => {
+    console.log("ids change", loaded.ids);
+    // remove ids that will be loaded from the state
+    setIdsToLoad(loaded.ids.slice(loadGroupSize));
+    loadAnimeInfo([], loaded.ids.slice(0, loadGroupSize));
+  }, [loaded.ids]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setCanRequest(true);
+    }, 1000 * requestTimeOut);
+  }, []);
 
   return (
     <>
@@ -74,17 +149,14 @@ export function Animes({addAnime}) {
           />{" "}
           <button type="submit">Search</button>
         </Form>
-        {loaded.animes.length > 0 ? (
+        {animes.length > 0 ? (
           <nav>
-            <ul>
-              {
-              loaded.animes.map((anime) => {
+            <ul className="search_results" onScroll={checkScroll}>
+              {animes.map((anime) => {
                 return (
-                  <li
-                    key={anime.id}
-                    onClick={()=>addAnime(anime)}
-                  >
+                  <li key={anime.id}>
                     <Anime anime={anime}></Anime>
+                    <button onClick={() => addAnime(anime)}>add to list</button>
                   </li>
                 );
               })}
